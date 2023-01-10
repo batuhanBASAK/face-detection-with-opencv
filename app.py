5db1d864d83b626aaa3676eeb04b112ba0da6349
batuhanBASAK <batuhanbasak1997@gmail.com>
2023-01-10 17:38:51 +0300

    bug fixed

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import cv2
 
 
 cap = cv2.VideoCapture(0) # 0 is the index of camera you want to use
+cap.set(cv2.CAP_PROP_FPS, 60) # set 60 fps
 
 face_cascade = cv2.CascadeClassifier("./haarcascade_frontalface_default.xml")
 
@@ -16,12 +17,12 @@ while True:
         # draw a rectangle around the face
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
     
-        # show the frame in window
-        cv2.imshow("frame", frame)
+    # show the frame in window
+    cv2.imshow("frame", frame)
 
-        # press q to exit the program
-        if cv2.waitKey(1) and 0xFF == ord("q"):
-            break
+    # press q to exit the program
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()

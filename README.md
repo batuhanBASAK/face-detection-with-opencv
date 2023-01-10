98e8cb0d5133a6c14c73a6fcc66a03cb5a016239
batuhanBASAK <batuhanbasak1997@gmail.com>
2023-01-10 16:47:52 +0300

    demo

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # face-recognition-with-opencv
+face recognition application to learn opencv2
---
+++ app.py
@@ -0,0 +1,29 @@
+import cv2
+
+
+cap = cv2.VideoCapture(0) # 0 is the index of camera you want to use
+
+face_cascade = cv2.CascadeClassifier("./haarcascade_frontalface_default.xml")
+
+while True:
+    ret, frame = cap.read() # ret - True if the frame was read successfuly
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, 
+    minNeighbors=5)
+
+    for (x, y, w, h) in faces:
+        # draw a rectangle around the face
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    
+        # show the frame in window
+        cv2.imshow("frame", frame)
+
+        # press q to exit the program
+        if cv2.waitKey(1) and 0xFF == ord("q"):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
+
+

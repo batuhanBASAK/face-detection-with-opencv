17152910e01361644cee1dcf8d65ca9e50ecee2f
batuhanBASAK <batuhanbasak1997@gmail.com>
2023-01-10 18:33:08 +0300

    bug fixed

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ import cv2
 cap = cv2.VideoCapture(0) # 0 is the index of camera you want to use
 cap.set(cv2.CAP_PROP_FPS, 60) # set 60 fps
 
+
 face_cascade = cv2.CascadeClassifier("./haarcascade_frontalface_default.xml")
 
 while True:
     ret, frame = cap.read() # ret - True if the frame was read successfuly
+    if not ret:
+        break
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, 
@@ -19,6 +22,7 @@ while True:
     
     # show the frame in window
     cv2.imshow("frame", frame)
+
 
     # press q to exit the program
     if cv2.waitKey(1) & 0xFF == ord('q'):
